14a9cad3c832261e7b32d0292b422bcd342fc63c
AnotherPillow <85362273+AnotherPillow@users.noreply.github.com>
2024-06-04 20:26:41 +0100

    Add image alt and make classes/IDs unique

--- widget.js
+++ widget.js
@@ -1,20 +1,20 @@
 (function () {
     const css = `
         @import url("https://fonts.googleapis.com/css?family=Lato:400,400i,700");
-        .circle {
+        .dsc-widget-circle {
             width: 0.7em;
             height: 0.7em;
             margin: 0.3em;
             border-radius: 50%;
         }
-        .circle.circle-online {
+        .dsc-widget-circle.circle-online {
             background-color: #23A559;
         }
-        .circle.circle-members {
+        .dsc-widget-circle.circle-members {
             background-color: #80848E;
         }
         
-        #invite-container {
+        #dsc-widget-invite-container {
             height: 16em;
             width: 40em;
             margin: 0;
@@ -29,16 +29,16 @@
             text-align: left;
         }
         
-        #child1, #child2 {
+        #dsc-widget-child1, #child2 {
             width: 100%;
             height: 100%;
         }
         
-        #child1 {
+        #dsc-widget-child1 {
             grid-row: 1/2;
         }
         
-        #child2 {
+        #dsc-widget-child2 {
             grid-row: 2/3;
             background-color: #1a1a1a;
             display: flex;
@@ -46,14 +46,14 @@
             gap: 16px;
             position: relative;
         }
-        #child2 #invite-notification {
+        #dsc-widget-child2 #idsc-widget-nvite-notification {
             margin: 0;
             position: absolute;
             left: calc(1.2em + 2px);
             top: 0.5em;
             color: gray;
         }
-        #child2 #server-info {
+        #dsc-widget-child2 #dsc-widget-server-info {
             display: flex;
             flex: 1000 0 auto;
             align-items: center;
@@ -61,7 +61,7 @@
             gap: 16px;
             margin-left: 1em;
         }
-        #child2 #server-info #server-icon {
+        #dsc-widget-child2 #server-info #dsc-widget-server-icon {
             flex: 0 0 auto;
             width: 5em;
             height: 5em;
@@ -71,7 +71,7 @@
             background-position: center center;
             background-size: 100% 100%;
         }
-        #child2 #server-info #server-details {
+        #dsc-widget-child2 #dsc-widget-server-info #dsc-widget-server-details {
             display: flex;
             flex: 1 1 auto;
             flex-direction: column;
@@ -81,26 +81,26 @@
             min-width: 1px;
             color: white;
         }
-        #child2 #server-info #server-details h3 {
+        #dsc-widget-child2 #dsc-widget-server-info #dsc-widget-server-details h3 {
             margin: 0;
         }
-        #child2 #server-info #server-members #member-counts-outer-parent {
+        #dsc-widget-child2 #dsc-widget-server-info #dsc-widget-server-members #dsc-widget-member-counts-outer-parent {
             display: flex;
             flex-flow: row wrap;
             align-items: center;
             column-gap: 12px;
         }
-        #child2 #server-info #server-members #member-counts-outer-parent .member-count {
+        #dsc-widget-child2 #dsc-widget-server-info #dsc-widget-server-members #dsc-widget-member-counts-outer-parent .dsc-widget-member-count {
             display: flex;
             flex: 0 1 auto;
             align-items: center;
             flex-flow: nowrap;
             min-width: 0;
         }
-        #child2 #server-info #server-members #member-counts-outer-parent .member-count span {
+        #dsc-widget-child2 #dsc-widget-server-info #dsc-widget-server-members #mdsc-widget-ember-counts-outer-parent .dsc-widget-member-count span {
             flex: 0 1 auto;
         }
-        #child2 #join-link-container {
+        #dsc-widget-child2 #dsc-widget-join-link-container {
             flex: 1 0 auto;
             align-self: center;
             height: inherit;
@@ -110,10 +110,10 @@
             align-items: center;
             margin-right: 1em;
         }
-        #child2 #join-link {
+        #dsc-widget-child2 #dsc-widget-join-link {
             text-decoration: none;
         }
-        #child2 #join-btn {
+        #dsc-widget-child2 #dsc-widget-join-btn {
             font-size: 1.5em;
             width: 5em;
             height: 2.5em;
@@ -124,51 +124,52 @@
             border: none;
             border-radius: 0.4em;
         }
-        #child2 #join-btn:hover {
+        #dsc-widget-child2 #dsc-widget-join-btn:hover {
             filter: brightness(0.8);
         }
         
     `
     const html = `
-    <div id="invite-container">
-    <div id="child1"> 
-    
-    </div>
-    <div id="child2"> 
-      <h3 id="invite-notification">You've been invited to join a server</h3>
-      <div id="server-info"> 
-        <div id="server-icon"></div>
-        <div id="server-details"> 
-          <h3 id="server-name">Loading...</h3>
-          <span id="server-members">
-            <div id="member-counts-outer-parent">
-              <div id="online-members" class="member-count">
-                <span class="circle circle-online">&nbsp;</span>
-                <span id="online-members-text" />
-              </div>
-              
-              <div id="total-members" class="member-count">
-                <span class="circle circle-members">&nbsp;</span>
-                <span id="total-members-text" />
-              </div>
-            </div>
-          </span>
+    <div id="dsc-widget-invite-container">
+        <div id="dsc-widget-child1"> 
+        
         </div>
-      </div>
-      <div id="join-link-container">
-        <a id="join-link" href="https://discord.gg" target="_blank">
-          <button id="join-btn">
-            Join
-          </button>
-        </a>
-      </div>
-      
-    </div>
-  </div>
+        <div id="dsc-widget-child2"> 
+            <h3 id="idsc-widget-nvite-notification">You've been invited to join a server</h3>
+            <div id="dsc-widget-server-info"> 
+                <div id="dsc-widget-server-icon"></div>
+                <div id="dsc-widget-server-details"> 
+                <h3 id="dsc-widget-server-name">Loading...</h3>
+                <span id="dsc-widget-server-members">
+                    <div id="dsc-widget-member-counts-outer-parent">
+                    <div id="dsc-widget-online-members" class="member-count">
+                        <span class="dsc-widget-circle dsc-widget-circle-online">&nbsp;</span>
+                        <span id="dsc-widget-online-members-text" />
+                    </div>
+                    
+                    <div id="dsc-widget-total-members" class="dsc-widget-member-count">
+                        <span class="dsc-widget-circle dsc-widget-circle-members">&nbsp;</span>
+                        <span id="dsc-widget-total-members-text" />
+                    </div>
+                    </div>
+                </span>
+                </div>
+            </div>
+            <div id="dsc-widget-join-link-container">
+                <a id="dsc-widget-join-link" href="https://discord.gg" target="_blank">
+                    <button id="dsc-widget-join-btn">
+                        Join
+                    </button>
+                </a>
+            </div>
+        
+        </div>
+    </div> 
     `
 
     document.addEventListener('DOMContentLoaded', () => {
         const style = document.createElement('style')
+        style.id = 'discord-widget-styling'
         style.innerHTML = css
         document.head.appendChild(style)
     })
@@ -185,30 +186,34 @@
          * 
          * @param {string} _invite 
          * @param {HTMLElement} element 
+         * @param {string?} fallbackImg fallback image URL alt
          */
-        constructor(_invite, element) {
+        constructor(_invite, element, fallbackImg = '') {
             this.container = element
             this.invite = _invite
+            this.altImg = fallbackImg
         }
 
         async create() {
             this.container.innerHTML = html
 
             
-            const inviteContainer = document.querySelector('#invite-container')
+            const inviteContainer = document.querySelector('#dsc-widget-invite-container')
                     
-            const iconElement = document.querySelector('#server-icon')
-            const nameElement = document.querySelector('#server-name')
-            const inviteElement = document.querySelector('#join-link')
+            /** @type {HTMLImageElement | null} */
+            const iconElement = document.querySelector('#dsc-widget-server-icon')
+            const nameElement = document.querySelector('#dsc-widget-server-name')
+            const inviteElement = document.querySelector('#dsc-widget-join-link')
             
-            const onlineMembersText = document.querySelector('#online-members-text')
-            const totalMembersText = document.querySelector('#total-members-text')
+            const onlineMembersText = document.querySelector('#dsc-widget-online-members-text')
+            const totalMembersText = document.querySelector('#dsc-widget-total-members-text')
             
             const data = await fetch(`https://discord.com/api/v9/invites/${this.invite}?with_counts=true&with_expiration=true`).then(res => res.json())
             
             const iconURL = `https://cdn.discordapp.com/icons/${data.guild.id}/${data.guild.icon}.png`
             
             iconElement.style.backgroundImage = `url('${iconURL}')`
+            iconElement.alt = this.altImg || ''
             
             const memberCount = data.approximate_member_count;
             totalMembersText.innerHTML = `${insertCommas(memberCount)} Members`
